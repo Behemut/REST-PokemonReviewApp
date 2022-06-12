fd5db702f8c6023f3fa1dc7504103b0f0f7a35c6
Kevin Pleitez <43393464+Behemut@users.noreply.github.com>
2022-06-12 13:18:26 -0600

    Saving API REST
    Creating first Endpoint using Mappers (AutoMapper framework Nuget)
    Creating Models, Controllers & implementing Repository Pattern

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.DTO;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IMapper _mapper;
+        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper  )
+        {
+            _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(ICollection<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            //var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
+            var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
+            
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int id)
+        {
+            if (!_pokemonRepository.PokemonExists(id))
+            {
+                return NotFound();
+            }
+            //var pokemon = _pokemonRepository.GetPokemon(id);
+            var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(id));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+
+
+
+
+        
+
+
+    }
+        
+}
---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        [Key]
+        public int Id{ get; set; }
+        [Required]
+        public string Name{ get; set; }
+        [Required]
+        public DateTime Birthdate { get; set; }
+
+        //ONE TO MANY RELATIONSHIP
+        public ICollection<Review> Reviews { get; set; }
+
+        //MANY TO MANY RELATIONSHIP
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory>  PokemonCategories { get; set; }
+
+    }
+}
